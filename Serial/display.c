a2170a98ee885df934498dfcf839b8903067ee36
Samuel Plane <samplaneis@hotmail.com>
2023-09-11 15:57:31 +0000

    Added constants

--- Serial/display.c
+++ Serial/display.c
@@ -1,6 +1,12 @@
 #include "stdio.h"
 
 int main() {
+
+  const int GRID_WIDTH = 15;
+  const int GRID_HEIGHT = 15;
+
+  //Create structure with malloc()
+  //Initialise with - using nested-for loop
   int gen = 0;
   printf("Generation: %d", gen);
   
