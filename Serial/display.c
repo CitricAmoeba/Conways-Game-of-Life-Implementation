1529548385cb543324807a5804ea4506d1c73959
unknown <samplaneis@hotmail.com>
2023-09-11 19:39:27 +0100

    Allocated and Deallocated Memory for 2D Structure

--- Serial/display.c
+++ Serial/display.c
@@ -5,9 +5,23 @@ int main() {
   const int GRID_WIDTH = 15;
   const int GRID_HEIGHT = 15;
 
-  //Create structure with malloc()
+  //Create 2D structure with 
+  char char **grid = malloc(GRID_HEIGHT * sizeof(*grid));
+  int i;
+  for (int i = 0; i < GRID_HEIGHT; i++) {
+  	grid[i] = malloc(GRID_WIDTH * sizeof(*grid[i]));
+  }
+
   //Initialise with - using nested-for loop
+  
+
   int gen = 0;
   printf("Generation: %d", gen);
+
+  //Deallocate memory
+  for (i = 0; i < GRID_HEIGHT; i++) {
+  	free(grid[i]);
+  }
+  free(grid);
   
 }
